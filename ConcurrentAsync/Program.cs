de9bb6887dcc312f5bb5dd5a2cf0421609c85978
Ke Yang <ke@feslen.com>
2018-08-18 23:05:16 +1200

    add Concurrent Async add Polymorphism

---
+++ ConcurrentAsync/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConcurrentAsync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            ConcurAsync ObjCA = new ConcurAsync();
+
+            ObjCA.DoMultipleTasks().Wait();
+
+            Console.ReadLine();
+        }
+    }
+
+    class ConcurAsync
+    {
+        public async Task DoMultipleTasks()
+        {
+            Task[] tasks = new Task[3];
+
+            tasks[0] = DoTaskWithTime("A", 8);
+            tasks[1] = DoTaskWithTime("B", 6);
+            tasks[2] = DoTaskWithTime("C", 2);
+
+            Console.WriteLine("All Tasks Started");
+            await Task.WhenAll(tasks);
+            Console.WriteLine("All Done");
+
+        }
+
+        public async Task DoTaskWithTime(string name, int secs)
+        {
+            Console.WriteLine("Start Task " + name + " needs " + secs + " secs to complete");
+            await Task.Delay(secs * 1000);
+            Console.WriteLine("End Task " + name);
+        }
+    }
+}
---
+++ Polymorphism/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Reflection;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseClass ObjBase;
+            BaseClassV ObjBaseV;
+
+            ObjBase = new DerivedClass();
+            ObjBase.DoSomething();
+
+            ObjBaseV = new DerivedClassV();
+            ObjBaseV.DoSomething();
+
+            Console.ReadLine();
+        }
+
+        class BaseClass
+        {
+            public void DoSomething()
+            {
+                Console.WriteLine(MethodBase.GetCurrentMethod().Name + " in BaseClass");
+            }
+        }
+
+        class DerivedClass : BaseClass
+        {
+            public new void DoSomething()
+            {
+                Console.WriteLine(MethodBase.GetCurrentMethod().Name + " in DerivedClass");
+            }
+        }
+
+        class BaseClassV
+        {
+            public virtual void DoSomething()
+            {
+                Console.WriteLine(MethodBase.GetCurrentMethod().Name + " in BaseClassV");
+            }
+        }
+
+        class DerivedClassV : BaseClassV
+        {
+            public override void DoSomething()
+            {
+                Console.WriteLine(MethodBase.GetCurrentMethod().Name + " in DerivedClassV");
+            }
+        }
+    }
+}
