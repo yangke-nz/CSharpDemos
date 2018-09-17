ee3e6229537d86499be6462130f36fb97a542f37
Ke Yang <ke@feslen.com>
2018-09-17 23:04:20 +1200

    added Unity Dependency Injection Demo

---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Unity;
+
+namespace DependencyInjection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IUnityContainer unityContainer = new UnityContainer();
+            unityContainer.RegisterType<ReportMaker>();
+            unityContainer.RegisterType<IMsgServer, FastMsgServer>();
+            ReportMaker reportMaker = unityContainer.Resolve<ReportMaker>();
+
+            reportMaker.GenerateReport();
+
+            Console.ReadLine();
+        }
+    }
+
+    internal class FastMsgServer : IMsgServer
+    {
+        public void SendMsg()
+        {
+            Console.WriteLine("Report msg sent by FastMsgServer");
+        }
+    }
+
+    internal interface IMsgServer
+    {
+        void SendMsg();
+    }
+
+    internal class ReportMaker
+    {
+        private IMsgServer msgServer;
+
+        public ReportMaker(IMsgServer msgServer)
+        {
+            this.msgServer = msgServer;
+        }
+
+        public void GenerateReport()
+        {
+            Console.WriteLine("Report Generated");
+            msgServer.SendMsg();
+        }
+    }
+}
